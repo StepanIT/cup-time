5240d3947ef025f53255ef46d781d1cfdc979ba0
Степан Рудаков <19stepan94@mail.ru>
2024-08-04 16:28:08 +0500

    добавил favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter } from "react-router-dom"
 import { Footer } from "./modules/Footer"
-import { Header } from "./modules/Header"
 import { Main } from "./modules/Main"
+import { Header } from "./modules/Header"
 
 
 function App() {
--- src/modules/ProductModal.jsx
+++ src/modules/ProductModal.jsx
@@ -2,17 +2,9 @@ import Modal from "react-modal";
 import { API_URL } from "../const";
 import { useState } from "react";
 import { useCart } from "../context/CartContext";
+import s from "./ProductModal.module.css";
 
-const customStyles = {
-  content: {
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)",
-  },
-};
+
 
 Modal.setAppElement("#root");
 
@@ -42,26 +34,66 @@ const handleAddToCart = () => {
   return <Modal
     isOpen={isOpen}
     onRequestClose={onRequestClose}
-    style={customStyles}
-    contentLabel="Product Modal"
+    className={s.modal}
+    overlayClassName={s.overlay}
+    contentLabel={data.title}
     >
-      <h2>{data.title}</h2>
-      <img src={`${API_URL}${data.img}`} alt={data.title}/>
-      <p>{data.price}</p>
-      <ul>
+
+<img className={s.image} src={`${API_URL}${data.img}`} alt={data.title}/>
+
+<div className={s.content}>
+<div className={s.header}>
+<h2 className={s.title}>{data.title}({data.id})</h2>
+<p className={s.price}>{data.price}&nbsp;₽</p>
+</div>
+      <ul className={s.list}>
         {Object.entries(data.additional).map(([key, value]) => (
-          <li key={key}>
-            <strong>{key}:</strong> {value}
+          <li key={key} className={s.item}>
+            <span className={s.field}>{key}:</span>
+            <span className={s.value}>{value}</span>
           </li>
         ))}
       </ul>
-        <button onClick={handleDecrease}>-</button>
-        <input type="number" value={quantity} readOnly />
-        <button onClick={handleIncrease}>+</button>
+      <div className={s.footer}>
+<div className={s.count}>
+    <button className={s.btn} onClick={handleDecrease}>
+      <svg
+        width="36"
+        height="36"
+        viewBox="0 0 36 36"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg">
+        <rect x="0.5" y="0.5" width="35" height="35" rx="3.5" stroke="#B8B8B8"/>
+        <rect x="12" y="17" width="12" height="2" fill="#1D1C1D"/>
+      </svg>
+    </button>
+      <input className={s.number} type="number" value={quantity} readOnly />
+    <button className={s.btn} onClick={handleIncrease}>
+      <svg width="36"
+        height="36"
+        viewBox="0 0 36 36"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg">
+        <rect x="0.5" y="0.5" width="35" height="35" rx="3.5" stroke="#B8B8B8"/>
+        <rect x="12" y="17.25" width="12" height="1.5" fill="#1D1C1D"/>
+        <rect x="17.25" y="24" width="12" height="1.5" transform="rotate(-90 17.25 24)" fill="#1D1C1D"/>
+      </svg>
+    </button>
+  </div>
 
-
-        <button onClick={handleAddToCart}>Добавить в корзину</button>
-        <button onClick={onRequestClose}>Закрыть</button>
+        <button className={s.btnAddCart} onClick={handleAddToCart}>Добавить</button>
+</div>
+</div>
+    <button className={s.btnCloseCard} onClick={onRequestClose}>
+      <svg width="17"
+        height="17"
+        viewBox="0 0 17 17"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg">
+        <rect x="3.71228" y="12.1975" width="12" height="1.5" transform="rotate(-45 3.71228 12.1975)" fill="#B8B8B8"/>
+        <rect x="12.1976" y="13.2582" width="12" height="1.5" transform="rotate(-135 12.1976 13.2582)" fill="#B8B8B8"/>
+      </svg>
+    </button>
     </Modal>;
   
 };
