ddfa5bcaf92b1eff4a9fc7d3ef99a67d82a28ab9
Степан Рудаков <19stepan94@mail.ru>
2024-08-05 23:58:50 +0500

    исправляю ошибки

--- src/modules/Cart.jsx
+++ src/modules/Cart.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useCart } from "../context/CartContext";
-import { CartItem } from "./CartItem";
+import { CartItem } from "./CartItem.jsx";
 import { SkeletonLoader } from "./SceletonLoader";
 import { useOrder } from "../context/OrderContext";
 import { API_URL } from "../const";
